1ad09b56a097bbc614e45b43f3b02d2368482676
scardenasb <84429399+scardenasb@users.noreply.github.com>
2023-08-15 03:51:29 -0400

    update

---
+++ golang/longest_common_prefix.go
@@ -0,0 +1,15 @@
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	for i := 0; i < len(strs[0]); i++ {
+		letter := string(strs[0][i])
+		for j := 0; j < len(strs); j++ {
+			if i >= len(strs[j]) || string(strs[j][i]) != letter {
+				return strs[0][:i]
+			}
+		}
+	}
+	return strs[0]
+}
