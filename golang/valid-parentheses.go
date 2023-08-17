ccb6f335198870c77c6b5d293789af2b16f9a307
scardenasb <84429399+scardenasb@users.noreply.github.com>
2023-08-16 22:17:12 -0400

    update

---
+++ golang/valid-parentheses.go
@@ -0,0 +1,23 @@
+func isValid(s string) bool {
+	charsMap := make(map[rune]rune)
+	charsMap[')'] = '('
+	charsMap[']'] = '['
+	charsMap['}'] = '{'
+
+	newStr := []rune(s)
+	if len(newStr) == 0 {
+		return false
+	}
+
+	for i := 0; i < len(newStr); i++ {
+		if newStr[i] == ')' || newStr[i] == ']' || newStr[i] == '}' {
+			if i == 0 || newStr[i-1] != charsMap[newStr[i]] {
+				return false
+			} else {
+				newStr = append(newStr[:i-1], newStr[i+1:]...)
+				i -= 2
+			}
+		}
+	}
+	return len(newStr) == 0
+}
